c23b5fce97d336f918e1a5df0302eda4e2b8639e
Matthew Dunn <matthewdunn@Matthews-MacBook-Pro.local>
2015-08-19 12:13:47 -0600

    Added averages tracker

--- StudyTracker/app/src/main/java/com/matt/studytracker/ChartFragment.java
+++ StudyTracker/app/src/main/java/com/matt/studytracker/ChartFragment.java
@@ -1,5 +1,6 @@
 package com.matt.studytracker;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -37,20 +39,36 @@ public class ChartFragment extends Fragment {
     private float[] yData;
 
     private  String[] timeIntervalArray = {"Day", "Week", "Month", "Six Months", "All Time"};
+    private  String[] averagesTimeIntervalArray = {"Day", "Week", "Month", "Six Months" };
     private List<String> timeIntervalList;
+    private List<String> averageTimeIntervalList;
     private ArrayAdapter<String> pieChartSpinnerAdapter;
+    private ArrayAdapter<String> averagesSpinnerAdapter;
 
     private String selectedInterval = "Week";
+    private String selectedAveragesInterval  = "Week";
 
     public static final String SELECTED_INTERVAL = "selectedInterval";
+    public static final String SELECTED_AVERAGES_INTERVALS = "selectedAveragesInterval";
+
+    private ArrayList<String> xValsList;
+    private ArrayList<Entry> yValsList;
+
+    private static Toast toast;
+
+    private TextView hoursBreakdown;
+    private TextView subjectBreakdown;
+
+    private TextView averages;
+    private TextView averagesSubjects;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.chart_fragment, container, false);
-        //textView = (TextView) rootView.findViewById(R.id.minutes_info);
-        //textView.setText(getMinutesString());
 
         if(savedInstanceState != null){
             selectedInterval = savedInstanceState.getString(SELECTED_INTERVAL);
+            selectedAveragesInterval = savedInstanceState.getString(SELECTED_AVERAGES_INTERVALS);
         }
 
         timeIntervalList = new ArrayList<String>(Arrays.asList(timeIntervalArray));
@@ -62,6 +80,15 @@ public class ChartFragment extends Fragment {
                         timeIntervalList
                 );
 
+        //initializes hours breakdown textview
+        hoursBreakdown = (TextView) rootView.findViewById(R.id.hours_breakdown_hours);
+        subjectBreakdown = (TextView) rootView.findViewById(R.id.hours_breakdown_subjects);
+
+        //initializes averages textviews
+        averages = (TextView) rootView.findViewById(R.id.averages_hours);
+        averagesSubjects = (TextView) rootView.findViewById(R.id.averages_subjects);
+
+
         Spinner pieChartSpinner = (Spinner) rootView.findViewById(R.id.percent_time_frame_spinner);
         pieChartSpinner.setAdapter(pieChartSpinnerAdapter);
         pieChartSpinner.setSelection(1);
@@ -71,7 +98,39 @@ public class ChartFragment extends Fragment {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 selectedInterval = pieChartSpinnerAdapter.getItem(i);
+                //xData = getSubjectsArray();
+                //yData = getPercentages(getTotalMinutesArray(howFarBack), xData.length);
                 updatePieChart();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        final Spinner averagesTimeIntervalSpinner = (Spinner) rootView.findViewById(R.id.average_time_frame_spinner);
+        averageTimeIntervalList = new ArrayList<String>(Arrays.asList(averagesTimeIntervalArray));
+        averagesSpinnerAdapter =
+                new ArrayAdapter<String>(
+                        this.getActivity(),
+                        R.layout.subject_chooser,
+                        R.id.subject_chooser_textview,
+                        averageTimeIntervalList
+                );
+
+        averagesTimeIntervalSpinner.setAdapter(averagesSpinnerAdapter);
+        averagesTimeIntervalSpinner.setSelection(1);
+
+        //for when a different time interval to determine average times is selected
+        averagesTimeIntervalSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedAveragesInterval = averagesTimeIntervalSpinner.getSelectedItem().toString();
+
+                updateAverages();
+
             }
 
             @Override
@@ -85,6 +144,16 @@ public class ChartFragment extends Fragment {
         updatePieChart();
 
         return rootView;
+    }
+
+    public void updateHoursBreakdown(){
+        hoursBreakdown.setText(getHoursBreakdownString());
+        subjectBreakdown.setText(getSubjectsBreakdownString());
+    }
+
+    public void updateAverages(){
+        averages.setText(getAveragesString(selectedAveragesInterval));
+        averagesSubjects.setText(getSubjectsBreakdownString());
     }
 
     public void updatePieChart(){
@@ -109,8 +178,17 @@ public class ChartFragment extends Fragment {
             @Override
             public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
                 // display msg when value selected
-                if (e == null)
-                    return;
+                String data = xValsList.get(e.getXIndex()) + " " + String.format("%.1f", e.getVal() * 100) + " %";
+
+                Context context = ((MainActivity) getActivity());
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                if (toast != null)
+                    toast.cancel();
+
+                toast = Toast.makeText(context, data, duration);
+                toast.show();
             }
 
             @Override
@@ -125,26 +203,26 @@ public class ChartFragment extends Fragment {
         addData();
 
         Legend l = pieChart.getLegend();
-        l.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
-        l.setXEntrySpace(7);
-        l.setYEntrySpace(5);
-        l.setTextSize(12);
-        l.setFormSize(15);
         l.setEnabled(false);
+
+        updateHoursBreakdown();
     }
 
     private void addData(){
-
-        //int[]
-
-        ArrayList<Entry> yValsList = new ArrayList<Entry>();
-        for (int i = 0; i < yData.length; ++i){
-            yValsList.add(new Entry(yData[i], i));
-        }
-
-        ArrayList<String> xValsList = new ArrayList<String>();
+        xValsList = new ArrayList<String>();
         for(int i = 0; i < xData.length; ++i){
             xValsList.add(xData[i]);
+        }
+
+        yValsList = new ArrayList<Entry>();
+        int entryIndex = 0;
+        for (int i = 0; i < yData.length; ++i){
+            if(yData[i] != 0) {
+                yValsList.add(new Entry(yData[i], entryIndex));
+                entryIndex++;
+            }
+            else
+                xValsList.remove(entryIndex);
         }
 
         PieDataSet dataSet = new PieDataSet(yValsList, "subjects");
@@ -194,10 +272,12 @@ public class ChartFragment extends Fragment {
         for(int i = 0; i < (subjectSize); ++i) {
             if (historyCursor.moveToFirst()) {
                 do {
-                    if(historyCursor.getString(1).replaceAll("\\s+", "").equals(subjectArray[i].replaceAll("\\s+", ""))){
+                    if(historyCursor.getString(1).replaceAll("\\s+", "").equals(subjectArray[i].replaceAll("\\s+", ""))
+                            && historyCursor.getLong(DBAdapter.H_DATE_COLUMN) >= formattedDate){
                         String timeElapsed = historyCursor.getString(DBAdapter.H_TIME_ELAPSED_COLUMN);
                         ++timeLocations;
 
+                        //TODO enable more than 10 hours
                         minutes += (Character.getNumericValue(timeElapsed.charAt(0)) * 60);
                         minutes += (Character.getNumericValue(timeElapsed.charAt(2)) * 10);
                         minutes += (Character.getNumericValue(timeElapsed.charAt(3)));
@@ -249,22 +329,108 @@ public class ChartFragment extends Fragment {
         return percentages;
     }
 
-    private String getMinutesString(){
+    private String getSubjectsBreakdownString(){
         String[] subjects = getSubjectsArray();
-        float[] minutes = {0}; //getPercentages(getTotalMinutesArray(), subjects.length);
 
-        String info = "";
+        String subject = "";
 
-        for (int i = 0; i < subjects.length; ++i){
-            info += subjects[i] + "   " + String.format("%.1f",minutes[i]*100) + "\n";
+        for(int i = 0; i < subjects.length; i++){
+            subject += subjects[i] + ":" + "\n";
         }
 
+        subject += "Total:";
+
+        return subject;
+    }
+
+    private String getHoursBreakdownString() {
+        String[] subjects = getSubjectsArray();
+        int[] intMinutes = getTotalMinutesArray(selectedInterval);
+
+        float[] minutes = new float[intMinutes.length];
+
+        for (int i = 0; i < minutes.length; i++) {
+            minutes[i] = (float) intMinutes[i];
+        }
+
+        String info = "";
+        float total = 0;
+        for (int i = 0; i < subjects.length; ++i) {
+            total += minutes[i] / 60;
+            info += String.format("%.1f", minutes[i] / 60) + " hours" + "\n";
+        }
+
+        info += String.format("%.1f", total) + " hours";
+
         return info;
+    }
+
+    private int getAmountOfDays(){
+
+        int difference = 1;
+        Cursor historyCursor = ((MainActivity) getActivity()).myDB.getAllHistoryRows();
+
+        if(historyCursor.moveToFirst()){
+            String firstEntry = Long.toString(historyCursor.getLong(DBAdapter.H_DATE_COLUMN));
+
+            historyCursor.moveToLast();
+            String lastEntry = Long.toString(historyCursor.getLong(DBAdapter.H_DATE_COLUMN));
+
+            int firstYear = Integer.parseInt(firstEntry.substring(0, 4));
+            int firstMonth = Integer.parseInt(firstEntry.substring(4, 6));
+            int firstDay = Integer.parseInt(firstEntry.substring(6, 8));
+
+            int lastYear = Integer.parseInt(lastEntry.substring(0, 4));
+            int lastMonth = Integer.parseInt(lastEntry.substring(4, 6));
+            int lastDay = Integer.parseInt(lastEntry.substring(6, 8));
+
+            difference = (firstYear - lastYear) * 365;
+            difference += (firstMonth - lastMonth) * 31;
+            difference += (firstDay - lastDay);
+        }
+
+        return difference;
+    }
+
+    private String getAveragesString(String when){
+        double denominator = 1;
+        if(when.equals("Day"))
+            denominator = 1;
+        if(when.equals("Week"))
+            denominator = 7;
+        if(when.equals("Month"))
+            denominator = 31;
+        if(when.equals("Six Months"))
+            denominator = 182;
+
+        double amountOfDays =  (double) getAmountOfDays();
+        double timeSpan = amountOfDays/denominator;
+
+        if(timeSpan < 1)
+            timeSpan = 1;
+
+        int[] totalMinutes = getTotalMinutesArray("All Time");
+        String[] subjects = getSubjectsArray();
+
+        String averages = "";
+        double total = 0;
+
+        for(int i = 0; i < subjects.length; ++i){
+            double doubleTotalHours = (double) totalMinutes[i]/60;
+            total += doubleTotalHours/timeSpan;
+            averages += String.format("%.1f", doubleTotalHours/timeSpan) + " hours" + "\n";
+        }
+
+        averages += String.format("%.1f", total) + " hours" + "\n";
+
+        return averages;
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putString(SELECTED_INTERVAL, selectedInterval);
+        outState.putString(SELECTED_AVERAGES_INTERVALS, selectedAveragesInterval);
         super.onSaveInstanceState(outState);
     }
+
 }
